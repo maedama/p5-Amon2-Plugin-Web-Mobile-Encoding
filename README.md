327fb68b61a3cb501302ccf2c3cf4637c164a19c
maedama <klemensplatz@gmail.com>
2014-11-13 14:41:08 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Amon2::Plugin::Web::MobileEncode - It's new $module
+
+# SYNOPSIS
+
+    use Amon2::Plugin::Web::MobileEncode;
+
+# DESCRIPTION
+
+Amon2::Plugin::Web::MobileEncode is ...
+
+# LICENSE
+
+Copyright (C) maedama.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+maedama <klemensplatz@gmail.com>
---
+++ lib/Amon2/Plugin/Web/MobileEncode.pm
@@ -0,0 +1,39 @@
+package Amon2::Plugin::Web::MobileEncode;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Amon2::Plugin::Web::MobileEncode - It's new $module
+
+=head1 SYNOPSIS
+
+    use Amon2::Plugin::Web::MobileEncode;
+
+=head1 DESCRIPTION
+
+Amon2::Plugin::Web::MobileEncode is ...
+
+=head1 LICENSE
+
+Copyright (C) maedama.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+maedama E<lt>klemensplatz@gmail.comE<gt>
+
+=cut
+
